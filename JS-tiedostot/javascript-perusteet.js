ce9a54aff6cda58856d313da9b274429512b8b6e
Petri Nurmi <pete874@hotmail.com>
2022-09-12 09:18:47 +0300

    js

---
+++ JS-tiedostot/javascript-perusteet.js
@@ -0,0 +1,11 @@
+var = luku1, luku2, summa, erotus, osamaara, tulo;
+luku1 = 10;
+luku2 = 2;
+summa = luku1 + luku2;
+erotus = luku1 - luku2;
+osamaara = luku1 / luku2;
+tulo = luku1 * luku2;
+console.log("Summa on:" + summa);
+console.log("Erotus on:" + erotus);
+console.log("Osamäärä on:" + osamaara);
+console.log("Tulo on:" + tulo);
