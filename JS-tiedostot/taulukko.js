f9e2592b539229280ed39a889258cafb25800df6
Petri Nurmi <pete874@hotmail.com>
2022-09-14 11:28:25 +0300

    .

--- JS-tiedostot/taulukko.js
+++ JS-tiedostot/taulukko.js
@@ -2,8 +2,16 @@ function esimerkkiTaulukko() {
   var autot = [];
   var caarat = ["Tesla", "VW i3", "Hyundai Ionic"];
   autot.push("Jeep");
-  document.write(caarat[1] + " " + autot[0] + "<br />");
-  document.write(caarat + "<br />");
-  document.write(autot);
+  document.write("caara[1] = " + caarat[1] + " autot[0] = " + autot[0] + "<br />");
+  document.write("CAARAT = " + caarat + "<br />");
+  document.write("autot = " + autot + "<br />");
+  var siirrettava = caarat.pop();
+  document.write("Siirrettävä = " + siirrettava);
+  document.write("<br /> Caarat taulukko = " + caarat);
+  autot.unshift(siirrettava);
+  document.write("<br />Autot taulukko = " + autot);
+  caarat.push() = autot.pop();
+  document.write("<br />Caarat taulukko = " + caarat + "<br />Autot taulukko = " + autot);
+
 
 }
