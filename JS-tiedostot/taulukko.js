80abfb4374a39e5468c4a59399760a29fb37df5e
Petri Nurmi <pete874@hotmail.com>
2022-09-14 09:51:15 +0300

    js

---
+++ JS-tiedostot/taulukko.js
@@ -0,0 +1,9 @@
+function esimerkkiTaulukko() {
+  var autot = [];
+  var caarat = ["Tesla", "VW i3", "Hyundai Ionic"];
+  autot.push("Jeep");
+  document.write(caarat[1] + " " + autot[0] + "<br />");
+  document.write(caarat + "<br />");
+  document.write(autot);
+
+}
