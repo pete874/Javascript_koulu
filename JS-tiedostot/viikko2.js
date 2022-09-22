dab86fb17772ce14316fb7e6c24d12bb893edab6
Petri Nurmi <pete874@hotmail.com>
2022-09-22 13:21:02 +0300

    .

---
+++ JS-tiedostot/viikko2.js
@@ -0,0 +1,58 @@
+
+// Tehtävä 1:
+
+function tehtava1() {
+  var luku1 = document.getElementById('luku1').value;
+  var luku2 = document.getElementById('luku2').value;
+  var luku3 = document.getElementById('luku3').value;
+  document.getElementById('v1').innerHTML = "annoit luvut: " + luku1 + " " + luku2 + " " + luku3;
+  if (luku1 < luku2 && luku1 < luku3) {
+    if (luku2 < luku3) {
+      document.getElementById('v2').innerHTML = "luvut oikeassa järjestysessä " + luku1 + " " + luku2 + " " + luku3;
+    } else {document.getElementById('v2').innerHTML = "luvut oikeassa järjestysessä " + luku1 + " " + luku3 + " " + luku2}
+  } else if (luku2 < luku1 && luku2 < luku3) {
+    if (luku1 < luku3) {document.getElementById('v2').innerHTML = "luvut oikeassa järjestysessä " + luku2 + " " + luku1 + " " + luku3;
+  } else {document.getElementById('v2').innerHTML = "luvut oikeassa järjestysessä " + luku2 + " " + luku3 + " " + luku1}
+  }
+  else if (luku3 < luku1 && luku3 < luku2) {
+    if(luku1 < luku2) {
+      document.getElementById('v2').innerHTML = "luvut oikeassa järjestysessä " + luku3 + " " + luku1 + " " + luku2;
+    } else {
+      document.getElementById('v2').innerHTML = "luvut oikeassa järjestysessä " + luku3 + " " + luku2 + " " + luku1;
+    }
+  }
+}
+
+
+// Tehtävä 2:
+
+function tehtava2() {
+  var luku1 = document.getElementById('t2luku1').value;
+  var luku2 = document.getElementById('t2luku2').value;
+  var luku3 = document.getElementById('t2luku3').value;
+  var luku4 = document.getElementById('t2luku4').value;
+  var luku5 = document.getElementById('t2luku5').value;
+  var vastaus
+
+  if (luku1 < luku2 && luku3 < luku2 && luku4 < luku2 && luku5 < luku2) {
+    document.getElementById('v3').innerHTML = "Luku " + luku2 + " On suurin"
+  } else if (luku1 < luku3 && luku4 < luku3 && luku5 < luku3) {
+    document.getElementById('v3').innerHTML = "Luku " + luku3 + " On suurin"
+  } else if (luku1 < luku4 && luku5 < luku4) {
+    document.getElementById('v3').innerHTML = "Luku " + luku4 + " On suurin"
+  } else if (luku1 < luku5) {
+    document.getElementById('v3').innerHTML = "Luku " + luku5 + " On suurin"
+  } else {document.getElementById('v3').innerHTML = "Luku " + luku1 + " On suurin"}
+}
+
+// Tehtävä 3:
+
+function tehtava3() {
+  var luku1 = document.getElementById('t3luku1').value
+
+  if (luku1%2 == 0) {
+    document.getElementById('v4').innerHTML = "Luku on parillinen"
+  } else {document.getElementById('v4').innerHTML = "Luku on pariton"}
+}
+
+// Tehtävä 4
