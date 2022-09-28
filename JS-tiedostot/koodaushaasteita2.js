724597e44b7a8b91abeabd7384edd944f6f8ddfc
Petri Nurmi <pete874@hotmail.com>
2022-09-28 15:54:29 +0300

    .

--- JS-tiedostot/koodaushaasteita2.js
+++ JS-tiedostot/koodaushaasteita2.js
@@ -12,8 +12,35 @@ function haaste2() {
 }
 
 //koodaushaasteita2 / 3. haaste
+function haaste3() {
+var string = "ohjelmointi on hauskaa";
+var string2 = document.getElementById('luku3').value;
+const asearch = "a";
+const akorvataan = "4";
+const esearch = "e";
+const ekorvataan = "3";
+const osearch = "o";
+const okorvataan = "0";
+const isearch = "i";
+const ikorvataan = "1";
+const ssearch = "s";
+const skorvataan = "5";
+const vastaus = string.split(asearch).join(akorvataan);
+const vastaus1 = vastaus.split(esearch).join(ekorvataan);
+const vastaus2 = vastaus1.split(osearch).join(okorvataan);
+const vastaus3 = vastaus2.split(isearch).join(ikorvataan);
+const vastaus4 = vastaus3.split(ssearch).join(skorvataan);
 
+const vastaus00 = string2.split(asearch).join(akorvataan);
+const vastaus11 = vastaus00.split(esearch).join(ekorvataan);
+const vastaus22 = vastaus11.split(osearch).join(okorvataan);
+const vastaus33 = vastaus22.split(isearch).join(ikorvataan);
+const vastaus44 = vastaus33.split(ssearch).join(skorvataan);
 
+console.log(vastaus4)
+document.getElementById('v3').innerHTML = vastaus4;
+document.getElementById('v33').innerHTML = vastaus44;
+}
 
 //koodaushaasteita2 / 4. haaste
 
