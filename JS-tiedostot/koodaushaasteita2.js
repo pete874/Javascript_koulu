77b027d0e4037d5658f6bd511951bc41570b6cb3
Petri Nurmi <pete874@hotmail.com>
2022-09-28 14:34:38 +0300

    .

---
+++ JS-tiedostot/koodaushaasteita2.js
@@ -0,0 +1,22 @@
+// koodaushaasteita2 / 1. haaste
+
+
+
+
+
+
+// Koodaushaasteita2 / 2. haaste
+
+function haaste2() {
+  document.getElementById('v2').innerHTML = "*<br>**<br>***<br>****<br>*****";
+}
+
+//koodaushaasteita2 / 3. haaste
+
+
+
+//koodaushaasteita2 / 4. haaste
+
+function haaste4() {
+
+}
